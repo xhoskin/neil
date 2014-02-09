092fb7d77317e710f5d2a9908cfe29131cd72a3d
xhoskin <xhoskin@gmail.com>
2014-02-10 01:51:55 +0600

    weekly

---
+++ vimcasts.c
@@ -0,0 +1,7 @@
+﻿// #44. Search multiple files with :vimgrep
+:vimgrep /pattern/ %          //искать в текущем файле
+<C-r>/                        //вставить последнюю команду поиска
+a                             //
+                              //
+                              //
+                              //
